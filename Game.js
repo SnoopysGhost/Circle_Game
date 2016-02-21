bf6a5ca5eaa324cf56626e2f57d19d3a8964c1aa
Ruan Williams <r.williams.sg@gmail.com>
2016-02-21 23:12:14 +0200

    added the mousedown and mouse move event hadlers to drag circle but still buggy

--- Game.js
+++ Game.js
@@ -1,12 +1,10 @@
 var main = function() {
   var height = 700
-
-  var width = 1800
-
-  var coordinates = [
+      width = 1800
+      coordinates = [
                       [width/2,height/2]
-                    ]
-
+                    ];
+//create canvas (drawing area)
   var canvas = d3.select("body").append("svg")
                   .attr("class","Game_Area")
                                   .attr("width", width)
@@ -14,8 +12,8 @@ var main = function() {
                                   .append("g")
                                   .attr("transform", "translate(0,0)")
                                   .append("g");//move the whole chart !!!(but not the canvas?)
-
-  var circles = canvas.selectAll(circles)
+//create circle
+  var circle = canvas.selectAll("circle")
                     .data(coordinates)
                     .enter()
                       .append("circle")
@@ -23,34 +21,50 @@ var main = function() {
                       .attr("cx", function(d) {return d[0]})
                       .attr("cy", function(d) {return d[1]})
                       .attr("r", 10);
-
+//trace mouse pointer
   var coor =  $(document).mousemove (function(event) {
                   $('span').text(event.pageX + "," + event.pageY);
                 });
 
-  /*//Add labels with text elements
-  var labels = canvas.selectAll("text")
-                      .data(coordinates)
-                      .enter()
-                        .append("text")
-                        .text(function(d) {return "(" + d[0] + "," + d[1] + ")"}) //create (x,y) label for each data point
-                        .attr("x",function(d) {return d[0]}) //specify where the label should be
-                        .attr("y",function(d) {return d[1]}) //specify where the label should be
-                        .attr("font-family", "sans-serif")
-                        .attr("font-size","11px")
-                        .attr("fill", "Green");*/
-
   //Add labels with text elements
-  var labels = canvas.selectAll("text")
+  /*var labels = canvas.selectAll("text")
                       .data(coordinates)
                       .enter()
                         .append("text")
                         .text(function(d) {return "(" + d[0] + "," + d[1] + ")"}) //create (x,y) label for each data point
-                        .attr("x",function(d) {return d[0]}) //specify where the label should be
+                        .attr("x",function(d) {return d[0]+ 10}) //specify where the label should be
                         .attr("y",function(d) {return d[1]}) //specify where the label should be
                         .attr("font-family", "sans-serif")
                         .attr("font-size","11px")
-                        .attr("fill", "Green");
-};
+                        .attr("fill", "Green")
+                        .attr("class","XYcoordinates");*/
 
+  //Add label on mouseover circle
+  canvas.selectAll("circle")
+          .on("mouseover.tooltip", function() { 
+              canvas.select("text").remove();  //removes any previuos text elements           
+              canvas.append("text")
+                    .data(coordinates) //supose to come with circle element????
+                    .text(function(d) {return "(" + d[0] + "," + d[1] + ")"}) //create (x,y) label for each data point
+                    .attr("x",function(d) {return d[0] + 10}) //specify where the label should be
+                    .attr("y",function(d) {return d[1]}) //specify where the label should be
+                    .attr("font-family", "sans-serif")
+                    .attr("font-size","11px")
+                    .attr("fill", "Red")
+                    .attr("class","XYcoordinates");
+                    
+          });
+
+  //Remove the label as mouseout circle
+  canvas.selectAll("circle")
+          .on("mouseout.tooltip", function() { 
+              canvas.select("text")
+              .transition()
+              .duration(500)
+              .style("opacity", 0)
+              .attr("transform","translate(10,-10)")
+              .remove();  //removes text element over 500ms in a disapering manier                              
+          });
+
+};//end of main function
 $(document).ready(main);
